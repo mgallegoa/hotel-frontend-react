c43028459deaf38c346bf79b6f16539507fc2441
mgallegoa <manuelgallego2003@yahoo.com>
2025-09-20 08:53:00 -0500

    HOTEL-REACT : CODE : Add libraries, bootstrap for styles.

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 const router = createBrowserRouter([
   {
